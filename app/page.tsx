057ff4536ec2cd2b1bd28f224a02defa5ead9977
ryuichi <ryu@ryuichinoMacBook-Pro.local>
2024-07-19 20:22:34 +0900

    feat, refactor: next/navigationのrouterを使うので置き換え

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import image from "@/public/konoka2.jpg";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
       <p>こにちわ～</p>
       <Image src={image.src} alt="" width={500} height={500} />
     </main>
