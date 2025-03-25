e45d85c501ff13edae10d012492a4188056ea5ab
ryuichi iha <ryuichiiha@gmail.com>
2025-03-25 09:16:16 +0900

    お試しでmotionをつけたUIを置いてみる

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import image from "@/public/konoka2.jpg";
+import ThreeDimensionalRoom from "./Otameshi";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <p>こにちわ～</p>
-      <Image src={image.src} alt="" width={500} height={500} />
+      <ThreeDimensionalRoom />
     </main>
   );
 }
